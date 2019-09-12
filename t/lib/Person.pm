6011a8f113d5a777d30f69cd10467d535897454d
Macide Celik <macide.35cc@gmail.com>
2019-09-12 15:53:23 +0300

    Delete Person.pm

--- t/lib/Person.pm
+++
@@ -1,8 +0,0 @@
-use Red;
-
-model Person is rw {
-    has Int  $.id            is serial;
-    has Str  $.name          is column;
-    has      @.posts         is relationship({ .author-id }, model => 'Post');
-}
-
